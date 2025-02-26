a57059d0269aaf0b44a4cf6f1b3e4abbcfc69508
BrandonAlanDev <brandoncarabajal@gmail.com>
2025-02-26 00:03:48 -0300

    configs 3

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'/Portfolio2025/',
   plugins: [tailwindcss(),react()],
+  build:{
+    outDir:'build'
+  }
 })
